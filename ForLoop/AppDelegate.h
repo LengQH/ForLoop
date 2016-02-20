66c3a7cdfad7d9175ecd5fa8584c2613b4d30b12
LengQiuHui <1205632644@qq.com>
2016-02-20 10:59:08 +0800

    Initial Commit

---
+++ ForLoop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ForLoop
+//
+//  Created by ma c on 16/2/20.
+//  Copyright © 2016年 gdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ForLoop/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ForLoop
+//
+//  Created by ma c on 16/2/20.
+//  Copyright © 2016年 gdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
